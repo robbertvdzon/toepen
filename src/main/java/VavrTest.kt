b607dbae02404bd11664ec3235c291bd46b28348
robbert <robbert@vdzon.com>
2020-05-10 13:29:08 +0200

    vavr fx test

--- src/main/java/VavrTest.kt
+++ src/main/java/VavrTest.kt
@@ -1,19 +1,21 @@
 import io.vavr.control.Either
+import kotlinx.coroutines.*
 
 object VavrTest {
 
     @JvmStatic
     fun main(args: Array<String>) {
         test()
+
     }
 
     fun test() {
         val e: Either<Fout2, Goed> = Either.left(Fout2("error"))
-//        val r: Either<Fout, Goed> = Either.right(Goed("mijn waarde1"))
-        val r: Either<Fout, Goed> = Either.left(Fout("mijn waarde1"))
+        val r: Either<Fout, Goed> = Either.right(Goed("mijn waarde1"))
+//        val r: Either<Fout, Goed> = Either.left(Fout("mijn waarde1"))
         val g: Either<Fout, Goed> = Either.right(Goed("mijn waarde"))
 
-        val y = g.bind() // moet niet hier mogen
+//        val y = g.bind() // moet niet hier mogen
 
 
         val d = fx<Fout, Goed> {
@@ -29,15 +31,20 @@ object VavrTest {
     }
 }
 
-fun <A,B> fx(c: () -> B): Either<A, B> {
+fun <A,B> fx(c: suspend  () -> B): Either<A, B> {
+
     try {
-        return Either.right(c.invoke())
+        val r = runBlocking<B> {
+            c.invoke()
+        }
+        return Either.right(r)
+
     } catch (e: MyEx) {
         return Either.left(e.fout as A)
     }
 }
 
-fun Either<Fout, Goed>.bind(): Goed {
+suspend fun Either<Fout, Goed>.bind(): Goed {
     if (this.isLeft) {
         throw MyEx(this.left)
     }
