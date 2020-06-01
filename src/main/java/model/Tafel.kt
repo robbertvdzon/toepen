abea5fc2debc4f5a27b563ef9affa757d048de50
robbert <robbert@vdzon.com>
2020-06-01 11:21:25 +0200

    refactoring

--- src/main/java/model/Tafel.kt
+++ src/main/java/model/Tafel.kt
@@ -12,12 +12,9 @@ data class Tafel(
   val tafelWinnaar: String? = null,
   val gepauzeerd: Boolean = SpelContext.spelData.nieuweTafelAutoPause == true
 ) {
-//  fun findSpelersDieAfZijn() = spelersDieAfZijn.map { SpelContext.findSpeler(it) }.filter { it != null }.map { it as Speler }
   fun findOpkomer() = SpelContext.findSpeler(opkomer)
   fun findHuidigeSpeler() = SpelContext.findSpeler(huidigeSpeler)
-//  fun findToeper() = SpelContext.findSpeler(toeper)
   fun findSlagWinnaar() = SpelContext.findSpeler(slagWinnaar)
-//  fun findTafelWinnaar() = SpelContext.findSpeler(tafelWinnaar)
   fun updateSpeler(speler:Speler):Tafel {
     val spelers = spelers.map {if (it.id==speler.id) speler else it}.toMutableList()
     return this.copy(spelers = spelers)
