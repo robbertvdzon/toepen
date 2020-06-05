977848e1c6646dfcf7c5d5a8b023df324dd1cd58
robbert <robbert@vdzon.com>
2020-06-05 16:46:07 +0200

    refactoring

--- src/main/java/spelprocessor/Monkey.kt
+++ src/main/java/spelprocessor/Monkey.kt
@@ -1,8 +1,6 @@
 package spelprocessor
 
 import Toepen
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import model.CommandStatus
 import model.Speler
 import model.Tafel
@@ -14,8 +12,7 @@ import kotlin.concurrent.thread
 
 object Monkey {
   const val DELAY: Long = 10
-  var echteSpelData = CommandQueue.lastSpelData
-  val waitingSpelers: MutableSet<String> = HashSet<String>()
+  val waitingSpelers: MutableSet<String> = HashSet()
   val lock = Object()
 
   fun addWaitingSpeler(spelerId: String) {
@@ -36,26 +33,20 @@ object Monkey {
     }
   }
 
-  /*
-  TODO: deze functie kan vast mooier
-   */
   fun start() {
     val timer = Timer()
 
     thread(start = true) {
       while (true) {
         Thread.sleep(DELAY)
-        echteSpelData = CommandQueue.lastSpelData
-        var spelData = CommandQueue.lastSpelData // TODO: maak hier een val
-
-        spelData.tafels.filter { !it.gepauzeerd }.forEach {
-          spelData = CommandQueue.lastSpelData
+        CommandQueue.lastSpelData.tafels.filter { !it.gepauzeerd }.forEach {
+          val spelData = CommandQueue.lastSpelData
           val huidigeSpeler = it.findHuidigeSpeler(spelData)
           val huidigeGebruiker = spelData.findGebruiker(it.huidigeSpeler)
           if (huidigeSpeler != null && huidigeGebruiker != null && huidigeGebruiker.isMonkey && !hasWaitingSpeler(huidigeSpeler.id)) {
             addWaitingSpeler(huidigeSpeler.id)
             val tasknew: TimerTask = TimerSchedulePeriod(it, huidigeSpeler)
-            val delay: Long = echteSpelData.monkeyDelayMsec ?: 3000
+            val delay: Long = spelData.monkeyDelayMsec ?: 3000
             timer.schedule(tasknew, delay)
           }
         }
@@ -64,18 +55,13 @@ object Monkey {
   }
 
 
-  /*
-  TODO: deze functie kan vast mooier
-   */
   private fun speelMonkey(tafel: Tafel, speler: Speler) {
-
     var aantalPogingen = 0
-    while (tafel.huidigeSpeler?.equals(speler.id) ?: false && aantalPogingen < 50) {
+    while (tafel.huidigeSpeler?.equals(speler.id) == true && aantalPogingen < 50) {
       aantalPogingen++
       if (tafel.slagWinnaar == speler.id) {
         val res = CommandQueue.addNewCommand(PakSlagCommand(speler.id))
         if (res.status == CommandStatus.SUCCEDED) {
-//                    println("Pak slag : model.Speler ${speler.naam} ,  hand:${speler.kaarten}")
           Toepen.broadcastMessage()
         }
 
@@ -83,16 +69,14 @@ object Monkey {
       if (Util.nextInt(0, 100) == 0) {
         val res = CommandQueue.addNewCommand(ToepCommand(speler.id))
         if (res.status == CommandStatus.SUCCEDED) {
-//                    println("Toep : model.Speler ${speler.naam} ,  hand:${speler.kaarten}")
           Toepen.broadcastMessage()
         }
       }
       if (Util.nextInt(0, 1) == 0) {
-        val kaart = speler.kaarten?.getOrNull(Util.nextInt(0, 3))
+        val kaart = speler.kaarten.getOrNull(Util.nextInt(0, 3))
         if (kaart != null) {
           val res = CommandQueue.addNewCommand(SpeelKaartCommand(speler.id, kaart))
           if (res.status == CommandStatus.SUCCEDED) {
-//                        println("Speelkaart: model.Speler ${speler.naam} kaart: $kaart ,  hand:${speler.kaarten}")
             Toepen.broadcastMessage()
           }
         }
@@ -100,22 +84,18 @@ object Monkey {
       if (Util.nextInt(0, 1) == 0) {
         val res = CommandQueue.addNewCommand(GaMeeMetToepCommand(speler.id))
         if (res.status == CommandStatus.SUCCEDED) {
-//                    println("Ga mee: model.Speler ${speler.naam} ,  hand:${speler.kaarten}")
           Toepen.broadcastMessage()
         }
       }
       if (Util.nextInt(0, 1) == 0) {
         val res = CommandQueue.addNewCommand(PasCommand(speler.id))
         if (res.status == CommandStatus.SUCCEDED) {
-//                    println("Pas: model.Speler ${speler.naam} ,  hand:${speler.kaarten}")
           Toepen.broadcastMessage()
         }
       }
-
     }
     removeWaitingSpeler(speler.id)
   }
-
 
   class TimerSchedulePeriod(val tafel: Tafel, val speler: Speler) : TimerTask() {
     override fun run() {
